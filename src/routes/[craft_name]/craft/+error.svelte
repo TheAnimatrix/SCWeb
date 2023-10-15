c4521d5335dfe7876f97b9d5e21693433fa37424
adithya <aditya.a@sankhya.com>
2023-10-15 20:35:10 +0530

    minor changes

---
+++ src/routes/[craft_name]/craft/+error.svelte
@@ -0,0 +1,5 @@
+
+
+<div>
+    STFU
+</div>
---
+++ src/routes/[craft_name]/craft/item=[item]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import ProductItem from "../../../product_item.svelte";
+    let productB = {
+        name : "Lorem",
+        author : "Ipsum",
+        price : "<s style='font-weight: 500;font-size: 140%;line-height: 110%;text-decoration-line: line-through;color: #959595;'>₹2699</s><br/><span style='font-weight: 700;font-size: 200%;line-height: 110%;color: #fdfbff;'><span style='font-size:50%;color:var(--accent-color-1)'>from </span>₹1499</span>",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iste labore corruptiillo deserunt tempora expedita velit rem, totam omnis, quia ipsa. Nostrum quos asperiores, optio aliquam deserunt molestiae dignissimos.",
+        info : "Out of stock",
+        infoColor : ["#FF5C38", "#ffffff"],
+        rating : 4.1,
+        ratingCount : 3,
+        accent1 : "#FF7A00",
+        accent2 : "#FFCE85",
+        accent3 : "#FF5C38"
+    };
+
+    export let data;
+    console.log(data)
+    productB.name=data.craft_name;
+</script>
+
+<ProductItem {...productB} onClick={()=>console.log("clicked")}/>
